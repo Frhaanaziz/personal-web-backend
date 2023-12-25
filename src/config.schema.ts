adddb8df722282e034da27e0ecef114b1050f291
Farhan Aziz <farhan@aththariq.com>
2023-12-26 00:36:10 +0700

    Add new environment variables to config schema

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,4 +3,12 @@ import * as Joi from '@hapi/joi';
 export const configValidationSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
   PORT: Joi.number().required(),
+
+  FRONTEND_URL: Joi.string().required(),
+
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required(),
+
+  EMAIL_SECRET: Joi.string().required(),
+  RESEND_API_KEY: Joi.string().required(),
 });
