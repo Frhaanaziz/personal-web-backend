1c69330f073fbd6e0ec607ac2e44ad27ba428d65
Farhan Aziz <farhan@aththariq.com>
2023-12-25 22:56:44 +0700

    Add AuthModule and AuthController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: configValidationSchema,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
