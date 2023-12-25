143de37d1a809809d62f084588511e87388556fb
Farhan Aziz <farhan@aththariq.com>
2023-12-26 00:36:26 +0700

    Add Passport, Jwt, Prisma, and Resend modules to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { JwtStrategy } from './jwt.strategy';
+import { ResendModule } from 'nestjs-resend';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+    ResendModule.forRoot({
+      apiKey: process.env.RESEND_API_KEY,
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, PrismaService, JwtStrategy, UsersService],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
