9a91e4f8870b61ce56ef0f9c8d87e537933c5ec5
Farhan Aziz <farhan@aththariq.com>
2023-12-26 14:39:27 +0700

    Add loginGoogle endpoint to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { LoginGoogleDto } from './dto/login-google.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,11 @@ export class AuthController {
   @Post('/verify-email-token')
   verifyEmailToken(@Body('token') token: string) {
     return this.authService.verifyEmailToken(token);
+  }
+
+  @Post('/login-google')
+  loginGoogle(@Body() loginGooleDto: LoginGoogleDto) {
+    return this.authService.loginGoogle(loginGooleDto);
   }
 
   // @Post('/signin')
