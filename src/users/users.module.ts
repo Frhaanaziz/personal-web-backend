79e7a8f4f7b58408b744ed4857523a8ed0fb6d80
Farhan Aziz <farhan@aththariq.com>
2023-12-26 12:50:25 +0700

    Add ResendModule and JwtService to UsersModule imports and providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ResendModule } from 'nestjs-resend';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ResendModule.forRoot({
+      apiKey: process.env.RESEND_API_KEY,
+    }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtService],
+  exports: [PrismaModule],
 })
 export class UsersModule {}
