2448f9a85d2347e80e40483f5c6737cdeadc87ad
Farhan Aziz <farhan@aththariq.com>
2023-12-26 11:13:37 +0700

    Add UsersController, UsersModule, AuthController, UsersService, and AuthService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
 import { AuthModule } from './auth/auth.module';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { UsersService } from './users/users.service';
+import { AuthService } from './auth/auth.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -11,8 +17,10 @@ import { AuthModule } from './auth/auth.module';
       validationSchema: configValidationSchema,
     }),
     AuthModule,
+    UsersModule,
+    PrismaModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, AuthController],
+  providers: [AppService, UsersService, AuthService],
 })
 export class AppModule {}
