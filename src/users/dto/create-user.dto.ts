8304d2ad7eae0c636d635e02dfdb4d4b41733080
Farhan Aziz <farhan@aththariq.com>
2023-12-26 21:23:08 +0700

    Refactor CreateUserDto class

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,6 @@
 import { Prisma } from '@prisma/client';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-// model User {
-//     id             String    @id @default(cuid())
-//     name           String
-//     email          String    @unique
-//     emailVerified  Boolean   @default(false)
-//     image          String?
-//     hashedPassword String?
-//     role           String    @default("user")
-//     createdAt      DateTime  @default(now())
-//     updatedAt      DateTime  @updatedAt
-//     accounts       Account[]
-//   }
-
 export class CreateUserDto {
   id?: string;
 
