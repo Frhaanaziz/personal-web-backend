ed7a523f77d78e25ab325b6cfbdc3d5ad6e91d61
Farhan Aziz <farhan@aththariq.com>
2023-12-26 22:17:25 +0700

    Add AuthGuardModule to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,27 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
 import { ResendModule } from 'nestjs-resend';
 import { UsersService } from 'src/users/users.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthGuardModule } from './auth-guard/auth-guard.module';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
-    }),
     ResendModule.forRoot({
       apiKey: process.env.RESEND_API_KEY,
     }),
     PrismaModule,
+    AuthGuardModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, UsersService, JwtService],
-  exports: [JwtStrategy, PassportModule, ResendModule, JwtService],
+  providers: [AuthService, UsersService],
+  exports: [ResendModule],
 })
 export class AuthModule {}
