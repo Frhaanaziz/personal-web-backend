7af69ff00fe1cd3660da827200d623c96167034b
Farhan Aziz <farhan@aththariq.com>
2023-12-26 20:47:03 +0700

    Add sign-in functionality to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { LoginGoogleDto } from './dto/login-google.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/signin')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
 
   @Post('/signup')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
