f73c3dc1fe41e2206639f8c7f131927153197433
Farhan Aziz <farhan@aththariq.com>
2023-12-27 09:14:04 +0700

    Add utility and Prisma modules to KeywordsModule

--- src/keywords/keywords.module.ts
+++ src/keywords/keywords.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { KeywordsService } from './keywords.service';
 import { KeywordsController } from './keywords.controller';
+import { UtilityModule } from 'src/utility/utility.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [UtilityModule, PrismaModule],
   controllers: [KeywordsController],
   providers: [KeywordsService],
 })
