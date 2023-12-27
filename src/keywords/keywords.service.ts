91bcb8e9db1143e269817e3ed1c2ac5c3d7e22ec
Farhan Aziz <farhan@aththariq.com>
2023-12-27 10:13:44 +0700

    Update KeywordsService with new methods

--- src/keywords/keywords.service.ts
+++ src/keywords/keywords.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateKeywordDto } from './dto/create-keyword.dto';
 import { UpdateKeywordDto } from './dto/update-keyword.dto';
-import { Prisma } from '@prisma/client';
+import { Keyword, Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UtilityService } from 'src/utility/utility.service';
 
@@ -12,12 +12,58 @@ export class KeywordsService {
     private utilityService: UtilityService,
   ) {}
 
-  create(createKeywordDto: CreateKeywordDto) {
-    return 'This action adds a new keyword';
-  }
+  // create(createKeywordDto: CreateKeywordDto) {
+  //   return 'This action adds a new keyword';
+  // }
 
-  update(id: number, updateKeywordDto: UpdateKeywordDto) {
-    return `This action updates a #${id} keyword`;
+  // update(id: number, updateKeywordDto: UpdateKeywordDto) {
+  //   return `This action updates a #${id} keyword`;
+  // }
+
+  async findOne(
+    contentWhereUniqueInput: Prisma.KeywordWhereUniqueInput,
+  ): Promise<Keyword | null> {
+    return this.prismaService.keyword.findUnique({
+      where: contentWhereUniqueInput,
+    });
+  }
+  async findMany(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.KeywordWhereUniqueInput;
+    where?: Prisma.KeywordWhereInput;
+    orderBy?: Prisma.KeywordOrderByWithRelationInput;
+    include?: Prisma.KeywordInclude;
+  }): Promise<Keyword[]> {
+    const { skip, take, cursor, where, orderBy, include } = params;
+    return this.prismaService.keyword.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+      include,
+    });
+  }
+  async create(data: CreateKeywordDto): Promise<Keyword> {
+    return this.prismaService.keyword.create({
+      data,
+    });
+  }
+  async update(params: {
+    where: Prisma.KeywordWhereUniqueInput;
+    data: UpdateKeywordDto;
+  }): Promise<Keyword> {
+    const { where, data } = params;
+    return this.prismaService.keyword.update({
+      data,
+      where,
+    });
+  }
+  async delete(where: Prisma.KeywordWhereUniqueInput): Promise<Keyword> {
+    return this.prismaService.keyword.delete({
+      where,
+    });
   }
 
   findAll({ page }: { page: number }) {
