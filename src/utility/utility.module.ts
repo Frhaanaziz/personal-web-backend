58f826443083b65ae6e5b0fd47644d22223883d1
Farhan Aziz <farhan@aththariq.com>
2023-12-27 09:13:55 +0700

    Add export for UtilityService in UtilityModule

--- src/utility/utility.module.ts
+++ src/utility/utility.module.ts
@@ -5,5 +5,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   providers: [UtilityService],
+  exports: [UtilityService],
 })
 export class UtilityModule {}
