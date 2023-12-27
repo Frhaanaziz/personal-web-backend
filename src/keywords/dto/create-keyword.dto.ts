58aa6e21a6e15c50d7be8fc92c13ddcf96ed2fd3
Farhan Aziz <farhan@aththariq.com>
2023-12-27 10:13:06 +0700

    Add properties and validations to CreateKeywordDto

--- src/keywords/dto/create-keyword.dto.ts
+++ src/keywords/dto/create-keyword.dto.ts
@@ -1,1 +1,18 @@
-export class CreateKeywordDto {}
+import { Prisma } from '@prisma/client';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateKeywordDto {
+  id?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  keyword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  group: string;
+
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+  Content?: Prisma.ContentCreateNestedManyWithoutKeywordInput;
+}
