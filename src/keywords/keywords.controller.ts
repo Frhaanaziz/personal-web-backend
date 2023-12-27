f0d71a0bc9570ecad9c759a70b886795fb6ce685
Farhan Aziz <farhan@aththariq.com>
2023-12-27 09:14:30 +0700

    Refactor keywords controller and service

--- src/keywords/keywords.controller.ts
+++ src/keywords/keywords.controller.ts
@@ -1,34 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { KeywordsService } from './keywords.service';
 import { CreateKeywordDto } from './dto/create-keyword.dto';
 import { UpdateKeywordDto } from './dto/update-keyword.dto';
+import { FindAllKeywordsDto } from './dto/find-all-keywords.dto';
 
 @Controller('keywords')
 export class KeywordsController {
   constructor(private readonly keywordsService: KeywordsService) {}
 
-  @Post()
-  create(@Body() createKeywordDto: CreateKeywordDto) {
-    return this.keywordsService.create(createKeywordDto);
-  }
+  // @Post()
+  // create(@Body() createKeywordDto: CreateKeywordDto) {
+  //   return this.keywordsService.create(createKeywordDto);
+  // }
 
   @Get()
-  findAll() {
-    return this.keywordsService.findAll();
+  findAll(@Query() { page }: FindAllKeywordsDto) {
+    return this.keywordsService.findAll({ page: Number(page) });
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.keywordsService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.keywordsService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateKeywordDto: UpdateKeywordDto) {
-    return this.keywordsService.update(+id, updateKeywordDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateKeywordDto: UpdateKeywordDto) {
+  //   return this.keywordsService.update(+id, updateKeywordDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.keywordsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.keywordsService.remove(+id);
+  // }
 }
--- src/keywords/keywords.service.ts
+++ src/keywords/keywords.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateKeywordDto } from './dto/create-keyword.dto';
 import { UpdateKeywordDto } from './dto/update-keyword.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UtilityService } from 'src/utility/utility.service';
 
 @Injectable()
 export class KeywordsService {
+  constructor(
+    private prismaService: PrismaService,
+    private utilityService: UtilityService,
+  ) {}
+
   create(createKeywordDto: CreateKeywordDto) {
     return 'This action adds a new keyword';
-  }
-
-  findAll() {
-    return `This action returns all keywords`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} keyword`;
   }
 
   update(id: number, updateKeywordDto: UpdateKeywordDto) {
     return `This action updates a #${id} keyword`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} keyword`;
+  findAll({ page }: { page: number }) {
+    return this.utilityService.getPaginatedResult(page, 'Keyword');
   }
 }
