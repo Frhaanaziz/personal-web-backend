bbbad7be3435938282edd1b4957cd9831c35c7c5
Farhan Aziz <farhan@aththariq.com>
2023-12-27 08:44:34 +0700

    Refactor contents.controller.ts

--- src/contents/contents.controller.ts
+++ src/contents/contents.controller.ts
@@ -2,29 +2,14 @@ import { Body, Controller, Param, Patch, UseGuards } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 import { UpdateContentDto } from './dto/update-content.dto';
 import { AuthGuard } from 'src/auth/auth-guard/auth.guard';
-// import { CreateContentDto } from './dto/create-content.dto';
-// import { UpdateContentDto } from './dto/update-content.dto';
+import { Admin } from 'src/auth/admin.decorator';
 
 @UseGuards(AuthGuard)
 @Controller('contents')
 export class ContentsController {
   constructor(private readonly contentsService: ContentsService) {}
 
-  // @Post()
-  // create(@Body() createContentDto: CreateContentDto) {
-  //   return this.contentsService.create(createContentDto);
-  // }
-
-  // @Get()
-  // findAll() {
-  //   return this.contentsService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.contentsService.findOne(+id);
-  // }
-
+  @Admin()
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateContentDto: UpdateContentDto) {
     return this.contentsService.update({
@@ -32,9 +17,4 @@ export class ContentsController {
       data: updateContentDto,
     });
   }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.contentsService.remove(+id);
-  // }
 }
