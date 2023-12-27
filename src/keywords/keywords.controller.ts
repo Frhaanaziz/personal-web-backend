561fa477bf8ee4950aaa81a4dd9f40c9bee3f991
Farhan Aziz <farhan@aththariq.com>
2023-12-27 11:12:12 +0700

    Add create, update, and delete methods to KeywordsController

--- src/keywords/keywords.controller.ts
+++ src/keywords/keywords.controller.ts
@@ -7,6 +7,8 @@ import {
   BadRequestException,
   Post,
   Body,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 import { KeywordsService } from './keywords.service';
 import { CreateKeywordDto } from './dto/create-keyword.dto';
@@ -19,12 +21,6 @@ import { Admin } from 'src/auth/admin.decorator';
 @Controller('keywords')
 export class KeywordsController {
   constructor(private readonly keywordsService: KeywordsService) {}
-
-  @Admin()
-  @Post()
-  create(@Body() createKeywordDto: CreateKeywordDto) {
-    return this.keywordsService.create(createKeywordDto);
-  }
 
   @Get()
   findAll(@Query() findAllKeywordsDto: FindAllKeywordsDto) {
@@ -51,18 +47,26 @@ export class KeywordsController {
     return this.keywordsService.findOne({ id });
   }
 
-  // @Get('section')
-  // findBySection(@Query() { section }: FindAllKeywordsDto) {
-  //   return this.keywordsService.findBySection({ section });
-  // }
+  @Admin()
+  @Post()
+  create(@Body() createKeywordDto: CreateKeywordDto) {
+    return this.keywordsService.create(createKeywordDto);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateKeywordDto: UpdateKeywordDto) {
-  //   return this.keywordsService.update(+id, updateKeywordDto);
-  // }
+  @Admin()
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateKeywordDto: UpdateKeywordDto) {
+    return this.keywordsService.update({
+      where: { id },
+      data: updateKeywordDto,
+    });
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.keywordsService.remove(+id);
-  // }
+  @Admin()
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.keywordsService.delete({
+      id,
+    });
+  }
 }
