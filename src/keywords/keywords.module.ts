3648fb637461acf6202e34121fecb36f2f0e07b8
Farhan Aziz <farhan@aththariq.com>
2023-12-27 10:13:37 +0700

    Add AuthModule to imports in keywords.module.ts

--- src/keywords/keywords.module.ts
+++ src/keywords/keywords.module.ts
@@ -3,9 +3,10 @@ import { KeywordsService } from './keywords.service';
 import { KeywordsController } from './keywords.controller';
 import { UtilityModule } from 'src/utility/utility.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [UtilityModule, PrismaModule],
+  imports: [UtilityModule, PrismaModule, AuthModule],
   controllers: [KeywordsController],
   providers: [KeywordsService],
 })
