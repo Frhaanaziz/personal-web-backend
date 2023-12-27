e3799ca465f11caaf1054f3a48197f3853801f9e
Farhan Aziz <farhan@aththariq.com>
2023-12-27 10:34:07 +0700

    Add create content endpoint to ContentsController

--- src/contents/contents.controller.ts
+++ src/contents/contents.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Param, Patch, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ContentsService } from './contents.service';
 import { UpdateContentDto } from './dto/update-content.dto';
 import { AuthGuard } from 'src/auth/auth-guard/auth.guard';
 import { Admin } from 'src/auth/admin.decorator';
+import { CreateContentDto } from './dto/create-content.dto';
 
 @UseGuards(AuthGuard)
 @Controller('contents')
 export class ContentsController {
   constructor(private readonly contentsService: ContentsService) {}
+
+  @Admin()
+  @Post()
+  create(@Body() createContentDto: CreateContentDto) {
+    return this.contentsService.create(createContentDto);
+  }
 
   @Admin()
   @Patch(':id')
