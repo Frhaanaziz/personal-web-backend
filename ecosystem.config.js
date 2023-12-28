5ad2db802f5996a63235f4ee5241e0df7f0f7002
Farhan Aziz <farhan@aththariq.com>
2023-12-28 12:14:34 +0700

    Remove commented out code in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,6 @@ module.exports = {
       watch: false,
       instances: 1,
       exec_mode: 'fork',
-      //   instances: 'max',
-      //   exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
       },
