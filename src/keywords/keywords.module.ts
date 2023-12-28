2c0a4f79c79fd280e7ebafa42e0ecc22e9b76c38
Farhan Aziz <farhan@aththariq.com>
2023-12-28 21:52:58 +0700

    Update imports in keywords module

--- src/keywords/keywords.module.ts
+++ src/keywords/keywords.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { KeywordsService } from './keywords.service';
 import { KeywordsController } from './keywords.controller';
 import { UtilityModule } from '../utility/utility.module';
-import { AuthModule } from '../auth/auth.module';
+import { AuthGuardModule } from '../auth/auth-guard/auth-guard.module';
 
 @Module({
-  imports: [UtilityModule, AuthModule],
+  imports: [UtilityModule, AuthGuardModule],
   controllers: [KeywordsController],
   providers: [KeywordsService],
 })
